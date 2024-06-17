ed213ed5a794d877a4fbe8dc83b3276117ae55d0
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2024-06-17 16:10:28 +0500

    Updated

---
+++ index.ts
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const apilink: string  = "https://opentdb.com/api.php?amount=6&category=18&difficulty=easy&type=multiple";
+
+let fetchData = async (data: string) => {
+
+    let fetchQuiz = await fetch(data);
+    let res = await fetchQuiz.json();
+    return res.results;
+}
+
+let data = await fetchData(apilink);
+
+let startQuiz = async () => {
+ let score: number = 0;
+ //for username
+
+ let name = await inquirer.prompt({
+    message: "What is your name?",
+    type: "input",
+    name: "fname"
+ });
+
+for (let i = 1; i < 6; i++){
+    let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+
+    let ans = await inquirer.prompt({
+        type: "list",
+        name: "quiz",
+        message:data[i].question,
+        choices: answers.map((val:any)=> val)
+    });
+if(ans.quiz == data[i].correct_answer) {
+    ++score;
+}
+
+}
+
+console.log(`Dear ${chalk.green(name.fname)} Your score is: ` + score);
+};
+
+startQuiz();

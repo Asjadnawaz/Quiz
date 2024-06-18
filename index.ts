0720aeb0217a516927a6ae324c9c59ba4de8d534
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2024-06-18 18:44:21 +0500

    Updated

--- index.ts
+++ index.ts
@@ -15,34 +15,39 @@ let fetchData = async (data: string) => {
 let data = await fetchData(apilink);
 
 let startQuiz = async () => {
- let score: number = 0;
- //for username
+    
+    let score: number = 0;
+    //for username
 
- let name = await inquirer.prompt({
-    message: "What is your name?",
-    type: "input",
-    name: "fname"
- });
-
-for (let i = 1; i < 6; i++){
-    let answers = [...data[i].incorrect_answers, data[i].correct_answer];
-
-    let ans = await inquirer.prompt({
-        type: "list",
-        name: "quiz",
-        message:data[i].question,
-        choices: answers.map((val:any)=> val)
+    let name = await inquirer.prompt({
+        message: "What is your name?",
+        type: "input",
+        name: "fname"
     });
-if(ans.quiz == data[i].correct_answer) {
-    ++score;
-    console.log(chalk.bold.italic.green("Correct"));
-} else {
-    console.log(`The Correct Answer is ${chalk.bold.italic.red(data[i].correct_answer)}`)
-}
 
-}
+    for (let i = 1; i < 6; i++){
 
-console.log(`Dear ${chalk.green(name.fname)} Your score is: ` + score);
+        let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+
+        let ans = await inquirer.prompt({
+            type: "list",
+            name: "quiz",
+            message:data[i].question,
+            choices: answers.map((val:any)=> val)
+        });
+
+        if(ans.quiz == data[i].correct_answer) {
+
+            ++score;
+            console.log(chalk.bold.italic.green("Correct"));
+
+        } else {
+
+            console.log(`The Correct Answer is ${chalk.bold.italic.red(data[i].correct_answer)}`)
+        }
+    }
+
+    console.log(`Dear ${chalk.green(name.fname)} Your score is: ` + score);
 };
 
 startQuiz();
